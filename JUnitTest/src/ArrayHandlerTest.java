023598f393ab59520133cba44fd5985f9b0d1fb1
AndreasAAR <Andreas.ahrlund@gmail.com>
2020-08-21 23:34:06 +0200

    Testing

--- JUnitTest/src/ArrayHandlerTest.java
+++ JUnitTest/src/ArrayHandlerTest.java
@@ -1,16 +1,37 @@
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.Assert.assertTrue;
 
 public class ArrayHandlerTest {
+    private ArrayHandler ah;
+    String[] testarr;
+
+    @BeforeClass
+    public void init(){
+        ah = new ArrayHandler();
+        testarr = ah.getArray();
+    }
 
     @Test
     public void testMain(){
        ArrayHandler ah = new ArrayHandler();
        ah.setArray(new String[1]);
-       String[] testarr = ah.getArray();
-
         Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
-            String testStr = testarr[2];
+            String testStr = ah.getArray()[2];
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1}) // six numbers
+    void shouldCast(int number) {
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, ()-> {
+            String[] testStr = new String[number];
+           String t = testStr[2];
         });
     }
 

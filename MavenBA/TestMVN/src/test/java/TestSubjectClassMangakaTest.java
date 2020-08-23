3dd879857b2ac0dffb1a84a2596aeb95a52476ec
AndreasAAR <Andreas.ahrlund@gmail.com>
2020-08-23 14:14:25 +0200

    Started working on own website for web-prog course

---
+++ MavenBA/TestMVN/src/test/java/TestSubjectClassMangakaTest.java
@@ -0,0 +1,15 @@
+
+class TestSubjectClassMangakaTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void interact() {
+    }
+}
